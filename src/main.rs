d23b277e5a882c56e129d29bbb91cbe5ef149f17
Felix Sánchez <felixsanchez0124@gmail.com>
2023-11-02 22:09:40 -0500

    wgpu triangle example running

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,72 @@
-use glfw::Context;
+use rust_graphics::State;
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window:: WindowBuilder,
+};
+
 
 
 fn main() {
-    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    let (mut window, _events) = glfw.create_window(640, 480, "My First Triangle", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
-    window.make_current();
 
-    // Load the OpenGL function pointers
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-    // Now you can safely check the OpenGL version
-    unsafe {
-        let version = std::ffi::CStr::from_ptr(gl::GetString(gl::VERSION) as *const _)
-            .to_str()
-            .unwrap();
-        println!("OpenGL version: {}", version);
-    }
-
-    // ... (rest of your code)
+    pollster::block_on(run());
 }
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new(window).await;
+
+    event_loop.run(move |event, _, control_flow| match event {
+
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            state.window().request_redraw();
+        }
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == state.window.id() => {
+
+            if !state.input(event) {
+                // Si no ha sido manejado, podrías hacer un manejo adicional aquí.
+
+                match event {
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    _ => {}
+                }
+            }
+            
+        },
+        _ => {}
+    });
+}
+ 
