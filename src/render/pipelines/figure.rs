13f3142b83e757db50215972963a80f618bc1939
Felix Sánchez <felixsanchez0124@gmail.com>
2023-12-23 15:16:15 -0600

    add insntances to figure

--- src/render/pipelines/figure.rs
+++ src/render/pipelines/figure.rs
@@ -29,6 +29,66 @@ impl VertexTrait for Vertex {
     const QUADS_INDEX: Option<wgpu::IndexFormat> = Some(wgpu::IndexFormat::Uint16);
     const STRIDE: wgpu::BufferAddress = std::mem::size_of::<Self>() as wgpu::BufferAddress;
 }
+
+
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct Instance {
+    pub model: [[f32; 4]; 4],
+}
+
+impl Instance {
+
+    pub fn new(
+        position: cgmath::Vector3<f32>,
+        rotation: cgmath::Quaternion<f32>
+    ) -> Self {
+            Self {
+            model: (cgmath::Matrix4::from_translation(position) * cgmath::Matrix4::from(rotation)).into()
+        }
+    }
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Instance>() as wgpu::BufferAddress,
+            // We need to switch from using a step mode of Vertex to Instance
+            // This means that our shaders will only change to use the next
+            // instance when the shader starts processing a new instance
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: &[
+                // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
+                // for each vec4. We'll have to reassemble the mat4 in the shader.
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    // While our vertex shader only uses locations 0, and 1 now, in later tutorials, we'll
+                    // be using 2, 3, and 4, for Vertex. We'll start at slot 5, not conflict with them later
+                    shader_location: 5,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+                    shader_location: 6,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+                    shader_location: 7,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+                    shader_location: 8,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+            ],
+        }
+    }
+}
+ 
+
+
 pub struct FigureLayout {
     pub bind_group_layout: wgpu::BindGroupLayout,
 }
@@ -106,7 +166,7 @@ impl FigurePipeline {
                 module: shader,
                 entry_point: "vs_main",
                 buffers: &[
-                    Vertex::desc()
+                    Vertex::desc(), Instance::desc()
                 ],
             },
             fragment: Some(wgpu::FragmentState {
