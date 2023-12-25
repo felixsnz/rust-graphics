de3a6a27fb8339126a47de7cac7833a3fa96a13e
Felix Sánchez <felixsanchez0124@gmail.com>
2023-12-25 12:02:03 -0600

    cube meshing

--- src/render/mesh.rs
+++ src/render/mesh.rs
@@ -71,6 +71,62 @@ impl<V: Vertex> Mesh<V> {
         self.indices.push(start_index );    // d
     
     }
+
+    // Método para añadir un cubo al mesh.
+    pub fn push_cube(&mut self, cube: Cube<V>) {
+        let start_index = self.verts.len() as u16;
+    
+        // Añadir los 8 vértices del cubo.
+        self.verts.extend_from_slice(&[
+            cube.a, cube.b, cube.c, cube.d, // base inferior
+            cube.e, cube.f, cube.g, cube.h, // base superior
+        ]);
+    
+        // Añadir índices para las 12 triángulos (6 caras * 2 triángulos por cara).
+        // Asegúrate de que el orden de los vértices para cada triángulo sea coherente y orientado hacia afuera.
+        let indices = [
+        // Base inferior (vista desde arriba para orientación hacia afuera)
+        start_index, start_index + 2, start_index + 1,
+        start_index, start_index + 3, start_index + 2,
+
+        // Base superior (vista desde abajo para orientación hacia afuera)
+        start_index + 4, start_index + 5, start_index + 6,
+        start_index + 4, start_index + 6, start_index + 7,
+
+        // Lado frontal
+        start_index + 1, start_index + 5, start_index,
+        start_index + 5, start_index + 4, start_index,
+
+        // Lado derecho
+        start_index + 2, start_index + 6, start_index + 1,
+        start_index + 6, start_index + 5, start_index + 1,
+
+        // Lado trasero
+        start_index + 3, start_index + 7, start_index + 2,
+        start_index + 7, start_index + 6, start_index + 2,
+
+        // Lado izquierdo
+        start_index, start_index + 4, start_index + 3,
+        start_index + 4, start_index + 7, start_index + 3,
+    ];
+        self.indices.extend_from_slice(&indices);
+    }
+    
+
+    
+}
+
+
+// Definición de la estructura Cube.
+pub struct Cube<V: Vertex> {
+    pub a: V, pub b: V, pub c: V, pub d: V, // Vértices de la base inferior
+    pub e: V, pub f: V, pub g: V, pub h: V, // Vértices de la base superior
+}
+
+impl<V: Vertex> Cube<V> {
+    pub fn new(a: V, b: V, c: V, d: V, e: V, f: V, g: V, h: V) -> Self {
+        Self { a, b, c, d, e, f, g, h }
+    }
 }
 
 
